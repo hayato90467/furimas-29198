52e6b4c2615b01b7bd4c4837e4f57594493062ad
hayato90467 <hayato90467@gmail.com>
2020-09-06 18:26:10 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, format: {with: /\A^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]/}   
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format:  { with: /\A[ァ-ヶー－]+\z/ }
+  validates :date, presence: true
+
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname                          {""}
+    email                             {""}
+    password                          {"0000000"}
+    password_confirmation             {""}
+    date                              {""}
+    family_name_kana                  {""}
+    first_name_kana                   {""}
+    first_name                        {""}
+    family_name                       {""}
+  end
+end
