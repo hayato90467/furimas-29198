e8e96a4ba6e38188bdb71467bd46ad13a0c9912f
hayato90467 <hayato90467@gmail.com>
2020-09-10 08:53:05 +0900

    追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :item do
-  end
+    factory :user do
+      user_id                          { 'abe' }
+      image                            { 'kkkis@mail.com' }
+      name                              { ' 写真です' }
+      name_description                  { 'よろしく' }
+      new_category_id                  { '4' }
+      name_condition_id                { '4' }
+      send_burden_id                   { '4' }
+      send_day_id                       { '4' }
+      send_area_id                     { '4' }
+      price                            { '4' }
+   end
+ end
 end
