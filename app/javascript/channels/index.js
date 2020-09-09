da290c21714b7839e066c4a7298323ffc67c7dba
hayato90467 <hayato90467@gmail.com>
2020-09-09 20:17:00 +0900

    js挿入

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -2,17 +2,21 @@
 // Channel files must be named *_channel.js.
   
 window.addEventListener('load', function(){
-  let list = document.getElementById("price").value
+  let list = document.getElementById("price")
+
   list.addEventListener('input', function(){
-    console.log(list)
-    // window.addEventListener('load', function(){
-    //   let list = document.getElementById("price").value
-    //   list.addEventListener('input', function(){
-    //     console.log(list)
-    //     window.addEventListener('load', function(){
-    //       let list = document.getElementById("price").value
-    //       list.addEventListener('input', function(){
-    //         console.log(list)
- 
-})
+    let taxprice = document.getElementById("add-tax-price")
+    let profit = document.getElementById("profit")
+
+    // caluculate
+    let list = document.getElementById("price").value
+    let tax = Math.floor(list * 0.1)
+    let profits = Math.floor(list * 0.9)
+
+
+    // fill in 2 answers
+    document.getElementById("add-tax-price").textContent = tax
+    document.getElementById("profit").textContent = profits
+
+  })
 })
