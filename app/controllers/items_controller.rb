9d30d5d7640a767178a4824a2bc3d717d1ed9f2e
hayato90467 <hayato90467@gmail.com>
2020-09-12 10:09:12 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,15 +20,6 @@ class ItemsController < ApplicationController
      @item = Item.find(params[:id])
    end
 
-  #  def edit
-  #   @item = Item.find(params[:id])
-  # end
-
-  # def update
-  #   @item = Tweet.find(params[:id])
-  #   item.update(item_params)
-  # end
-
 
    private
 
