5c30971fc5ea4438733e0bede40ea8f61f3aa292
hayato90467 <hayato90467@gmail.com>
2020-09-19 17:39:01 +0900

    修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -55,7 +55,6 @@ describe PurchaseAddress do
      it '電話番号ハイフン不要で10〜11桁内でないとと保存できないこと' do
       @purchase_addresses.mobile = '00-31２'
       @purchase_addresses.valid?
-      binding.pry
       expect(@purchase_addresses.errors.full_messages).to include("Mobile is invalid")
      end
       it 'tokenがないと保存できない' do
