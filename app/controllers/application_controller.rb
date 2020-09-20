07c498c4f5e47f97a48ed2904d862917ef2d76ff
hayato90467 <hayato90467@gmail.com>
2020-09-20 21:39:00 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,
                                                        :family_name, :first_name_kana, :family_name_kana, :date])
   end
+  
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
